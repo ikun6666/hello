ed9c292eec2bb6ec368d21a95dd125b727905b99
llk <111@163.com>
2020-04-01 22:50:56 +0800

    v1

---
+++ src/main/java/com/codeduck/Hello.java
@@ -0,0 +1,13 @@
+package com.codeduck;
+
+/**
+ * @Version 1.0
+ * @Author: Lin Liangkun
+ * @Date: 2020/4/1
+ * @Content:
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
